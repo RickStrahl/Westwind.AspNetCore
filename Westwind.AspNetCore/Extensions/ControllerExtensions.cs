9a7f4f4c0981458c4b473de493bd1ec4c41aa145
Rick Strahl <rstrahl@west-wind.com>
2017-09-10 16:11:23 -1000

    Add project files.

---
+++ Westwind.AspNetCore/Extensions/ControllerExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Westwind.AspNetCore.Extensions
+{
+    public static class HttpRequestExtensions
+    {
+
+        /// <summary>
+        /// Retrieve the raw body as a string from the Request.Body stream
+        /// </summary>
+        /// <param name="request">Request instance to apply to</param>
+        /// <param name="encoding">Optional - Encoding, defaults to UTF8</param>
+        /// <returns></returns>
+        public static async Task<string> GetRawBodyStringAsync(this HttpRequest request, Encoding encoding = null)
+        {
+            if (encoding == null)
+                encoding = Encoding.UTF8;
+
+            using (StreamReader reader = new StreamReader(request.Body, encoding))
+                return await reader.ReadToEndAsync();
+        }
+
+        /// <summary>
+        /// Retrieves the raw body as a byte array from the Request.Body stream
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public static async Task<byte[]> GetRawBodyBytesAsync(this HttpRequest request)
+        {
+            using (var ms = new MemoryStream(2048))
+            {                
+                await request.Body.CopyToAsync(ms);
+                return ms.ToArray();
+            }
+        }
+    }
+}
---
+++ Westwind.AspNetCore/Formatters/RawRequestBodyStringFormatter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+
+namespace Westwind.AspNetCore.Formatters
+{
+
+    /// <summary>
+    /// Formatter that allows content of type text/plain and application/octet stream
+    /// or no content type to be parsed to raw data. Allows for a single input parameter
+    /// in the form of:
+    /// 
+    /// public string RawString([FromBody] string data)
+    /// public byte[] RawData([FromBody] byte[] data)
+    /// </summary>
+    public class RawRequestBodyFormatter : InputFormatter
+    {
+        public RawRequestBodyFormatter()
+        {
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/plain"));
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/octet-stream"));
+        }
+
+
+        /// <summary>
+        /// Allow text/plain, application/octet-stream and no content type to
+        /// be processed
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override Boolean CanRead(InputFormatterContext context)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+
+            var contentType = context.HttpContext.Request.ContentType;
+            if (string.IsNullOrEmpty(contentType) || contentType == "text/plain" ||
+                contentType == "application/octet-stream")
+                return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Handle text/plain or no content type for string results
+        /// Handle application/octet-stream for byte[] results
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context)
+        {
+            var request = context.HttpContext.Request;
+            var contentType = context.HttpContext.Request.ContentType;
+
+
+            if (string.IsNullOrEmpty(contentType) || contentType == "text/plain")
+            {
+                using (var reader = new StreamReader(request.Body))
+                {
+                    var content = await reader.ReadToEndAsync();
+                    return await InputFormatterResult.SuccessAsync(content);
+                }
+            }
+            if (contentType == "application/octet-stream")
+            {
+                using (var ms = new MemoryStream(2048))
+                {
+                    await request.Body.CopyToAsync(ms);
+                    var content = ms.ToArray();
+                    return await InputFormatterResult.SuccessAsync(content);
+                }
+            }
+
+            return await InputFormatterResult.FailureAsync();
+        }
+    }
+}
