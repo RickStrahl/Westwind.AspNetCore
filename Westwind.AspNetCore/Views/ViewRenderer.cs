c9c78f9396a63168a10d8549a7e8f65d3568b76f
rstrahl <rstrahl@west-wind.com>
2022-07-06 14:15:54 -0700

    Add exception for handling RazorEngine retrieval failure. - add operation to retrieve controller context or create one.

--- Westwind.AspNetCore/Views/ViewRenderer.cs
+++ Westwind.AspNetCore/Views/ViewRenderer.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Westwind.AspNetCore.Properties;
 
 namespace Westwind.AspNetCore.Views
 {
@@ -23,7 +26,7 @@ namespace Westwind.AspNetCore.Views
         /// <param name="controllerContext">A controller context needed to process this View</param>
         /// <param name="isPartial">Renders either a partial or main page (default)</param>
         /// <returns></returns>
-        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
         public static async Task<string> RenderViewToStringAsync(
             string viewName, object model,
             ControllerContext controllerContext,
@@ -33,22 +36,24 @@ namespace Westwind.AspNetCore.Views
             var serviceProvider = controllerContext.HttpContext.RequestServices;
 
             var razorViewEngine = serviceProvider.GetService(typeof(IRazorViewEngine)) as IRazorViewEngine;
+            if (razorViewEngine == null)
+                throw new InvalidOperationException( Resources.RazorViewEngineNotAvailable);
+
             var tempDataProvider = serviceProvider.GetService(typeof(ITempDataProvider)) as ITempDataProvider;
 
             using (var sw = new StringWriter())
             {
                 var viewResult = razorViewEngine.FindView(actionContext, viewName, !isPartial);
 
-                if (viewResult.View == null)
+                if (viewResult?.View == null)
                 {
-                    throw new ArgumentNullException($"{viewName} does not match any available view");
+                    throw new ArgumentException(string.Format(Resources.ViewNameDoesNotMatchAvailableView,viewName));
                 }
 
                 var viewDictionary =
                     new ViewDataDictionary(new EmptyModelMetadataProvider(),
                         new ModelStateDictionary())
                     { Model = model };
-
 
                 var viewContext = new ViewContext(
                     actionContext,
@@ -62,6 +67,26 @@ namespace Westwind.AspNetCore.Views
                 await viewResult.View.RenderAsync(viewContext);
                 return sw.ToString();
             }
+
+
+
+        }
+
+        /// <summary>
+        /// Creates a controller context from a base Url and without an Http Context
+        /// </summary>
+        /// <param name="baseUrl"></param>
+        /// <returns></returns>
+        public static ControllerContext CreateControllerContext(string baseUrl)
+        {
+            var httpContext = new DefaultHttpContext();
+
+            var currentUri = new Uri(baseUrl); // Here you have to set your url if you want to use links in your email
+            httpContext.Request.Scheme = currentUri.Scheme;
+            httpContext.Request.Host = HostString.FromUriComponent(currentUri);
+
+            var actionContext = new ActionContext(httpContext, new Microsoft.AspNetCore.Routing.RouteData(), new Microsoft.AspNetCore.Mvc.Abstractions.ActionDescriptor());
+            return  new ControllerContext(actionContext);
         }
     }
 
