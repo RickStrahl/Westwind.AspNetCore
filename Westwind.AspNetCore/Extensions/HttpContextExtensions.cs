3c4980be1eca719b31b765a1fdf5994ad2db932e
Rick Strahl <rstrahl@west-wind.com>
2024-02-17 20:52:59 -1000

    Add HttpContextExtensions.SetUserLocale() to allow detecting and setting browser's user locale and set on server.

--- Westwind.AspNetCore/Extensions/HttpContextExtensions.cs
+++ Westwind.AspNetCore/Extensions/HttpContextExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
+using System.Threading;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 
@@ -54,4 +56,107 @@ public static class HttpContextExtensions
             .Replace("\\", slash)
             .Replace(slash + slash, slash);
     }
+
+
+    /// <summary>
+    /// Sets the culture and UI culture to a specific culture. Allows overriding of currency
+    /// and optionally disallows setting the UI culture.
+    /// 
+    /// You can also limit the locales that are allowed in order to minimize
+    /// resource access for locales that aren't implemented at all.
+    /// </summary>
+    /// <param name="culture">
+    /// 2 or 5 letter ietf string code for the Culture to set. 
+    /// Examples: en-US or en</param>
+    /// <param name="uiCulture">ietf string code for UiCulture to set</param>
+    /// <param name="currencySymbol">Override the currency symbol on the culture</param>
+    /// <param name="setUiCulture">
+    /// if uiCulture is not set but setUiCulture is true 
+    /// it's set to the same as main culture
+    /// </param>
+    /// <param name="allowedLocales">
+    /// Names of 2 or 5 letter ietf locale codes you want to allow
+    /// separated by commas. If two letter codes are used any
+    /// specific version (ie. en-US, en-GB for en) are accepted.
+    /// Any other locales revert to the machine's default locale.
+    /// Useful reducing overhead in looking up resource sets that
+    /// don't exist and using unsupported culture settings .
+    /// Example: de,fr,it,en-US
+    /// </param>
+    public static void SetUserLocale(this HttpContext httpContext, string culture = null,
+        string uiCulture = null,
+        string currencySymbol = null,
+        bool setUiCulture = true,
+        string allowedLocales = null)
+    {
+        // Use browser detection in ASP.NET
+        if (string.IsNullOrEmpty(culture) && httpContext != null)
+        {
+            var request = httpContext.Request;
+
+            
+            // if no user lang leave existing but make writable
+            if (request.Headers.AcceptLanguage.Count < 1)
+            {
+                Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentCulture.Clone() as CultureInfo;
+                if (setUiCulture)
+                    Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentUICulture.Clone() as CultureInfo;
+
+                return;
+            }
+
+            culture = request.Headers.AcceptLanguage[0];
+        }
+        else
+            culture = culture.ToLower();
+
+        if (!string.IsNullOrEmpty(uiCulture))
+            setUiCulture = true;
+
+        if (!string.IsNullOrEmpty(culture) && !string.IsNullOrEmpty(allowedLocales))
+        {
+            allowedLocales = "," + allowedLocales.ToLower() + ",";
+            if (!allowedLocales.Contains("," + culture + ","))
+            {
+                int i = culture.IndexOf('-');
+                if (i > 0)
+                {
+                    if (!allowedLocales.Contains("," + culture.Substring(0, i) + ","))
+                    {
+                        // Always create writable CultureInfo
+                        Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentCulture.Clone() as CultureInfo;
+                        if (setUiCulture)
+                            Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentUICulture.Clone() as CultureInfo;
+
+                        return;
+                    }
+                }
+            }
+        }
+
+        if (string.IsNullOrEmpty(culture))
+            culture = CultureInfo.InstalledUICulture.IetfLanguageTag;
+
+        if (string.IsNullOrEmpty(uiCulture))
+            uiCulture = culture;
+
+        try
+        {
+            CultureInfo Culture = new CultureInfo(culture);
+
+            if (currencySymbol != null && currencySymbol != "")
+                Culture.NumberFormat.CurrencySymbol = currencySymbol;
+
+            Thread.CurrentThread.CurrentCulture = Culture;
+
+            if (setUiCulture)
+            {
+                var UICulture = new CultureInfo(uiCulture);
+                Thread.CurrentThread.CurrentUICulture = UICulture;
+            }
+        }
+        catch { }
+    }
+
+    
 }
